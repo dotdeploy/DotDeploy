f7ebf1317f0eead9acd8385b503a35032102d2a0
Spencer Thomas <spthomas@expedia.com>
2014-05-18 08:01:53 -0700

    Poller reads a manifest file, determines whether to update, add, or exit on conflict for all files in manifest

--- resources/public/lib/poller.sh
+++ resources/public/lib/poller.sh
@@ -1,17 +1,96 @@
 #!/bin/bash
 
 # Calls the service, grabs a manifest and looks for files to pull down. 
-
-DOTDEPLOY_URL="http://192.168.11.26:3000"
+DOTDEPLOY_URL="http://localhost:8000"
 DOTDEPLOY_DIRECTORY=$HOME/.dotdeploy
 MACHINE_UUID=$(cat $DOTDEPLOY_DIRECTORY/.machine_uuiid)
 
 # pull in explicit dependencies
+source $DOTDEPLOY_DIRECTORY/lib/logHelper.sh
 source $DOTDEPLOY_DIRECTORY/lib/httpClient.sh
 source $DOTDEPLOY_DIRECTORY/lib/fileMetadataStore.sh
 
-# perform call to service
-fetchUrl "$DOTDEPLOY_URL/api/machine/$MACHINE_UUID/manifest.csv" | while read manifestLine
+# which column in manifest to look in
+FILE_ID_FIELD=1
+PATH_FIELD=2
+REVISION_FIELD=3
+LOCATION_FIELD=4
+
+# $1: url
+# $2: path
+# $3: fileId
+# $4: revision
+function fetchFilePopulateMetadata {
+    url=$1
+    path=$2
+    fileId=$3
+    revision=$4
+    
+    fetchUrl $location > $DOTDEPLOY_DIRECTORY/tracked/$fileId
+    sha5Sum=$(sha5SumFileId $fileId)
+    
+    # update our metadata on it
+    echo "revision=$revision" > $DOTDEPLOY_DIRECTORY/tracked/$fileId.meta
+    echo "path=$path" >> $DOTDEPLOY_DIRECTORY/tracked/$fileId.meta
+    echo "sha5sum=$sha5sum" >> $DOTDEPLOY_DIRECTORY/tracked/$fileId.meta   
+}
+
+fetchUrl "$DOTDEPLOY_URL/manifest.csv" | while read manifestLine
 do
-    echo ""
+    # ignore if it's a comment (i.e. a header row)
+    if [[ ${manifestLine:0:1} == "#" ]]
+    then
+        continue
+    fi
+    
+    fileId=$(echo $manifestLine | cut -d, -f$FILE_ID_FIELD)
+    path=$(echo $manifestLine | cut -d, -f$PATH_FIELD)
+    revision=$(echo $manifestLine | cut -d, -f$REVISION_FIELD)
+    location=$(echo $manifestLine | cut -d, -f$LOCATION_FIELD)
+    
+    # determine if file already exists
+    getMetadataForId $fileId
+    if [ $? -eq 0 ]
+    then
+        if localEditsExist $fileId
+        then
+            logError "local changes exist in $path, not updating"
+            echo
+            exit 1
+        else 
+            # local edits do not exist, proceed with updating the file
+            fetchFilePopulateMetadata $location $path $fileId $revision
+        fi
+    else
+        # file is not tracked locally. back up existing file if necessary.
+        # then create a symbolic link replacing the old file with the new 
+        # tracked id.
+                
+        # can refactor this call outside of both conditionals
+        fetchFilePopulateMetadata $location $path $fileId $revision
+        if [ ! $? -eq 0 ]
+        then
+            logError "could not fetch file from $location, exiting"
+            echo
+            exit 1
+        fi
+        
+        if [ -a $path ]
+        then
+            # file currently exists on machine, put it into originals directory
+            relativePath=$(echo $path | sed s/~\\///)
+            destination=$DOTDEPLOY_DIRECTORY/originals
+            
+            mv $path $destination
+            
+            if [ ! $? -eq 0 ]
+            then
+                logError "could not move file from $path, exiting"
+                echo
+                exit 1
+            fi
+        fi
+                    
+        ln -s $DOTDEPLOY_DIRECTORY/tracked/$fileId $path
+    fi      
 done
