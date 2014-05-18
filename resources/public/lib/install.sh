5ede54f72b4e2119e248987848d065689f8ad9d0
Spencer Thomas <spthomas@expedia.com>
2014-05-17 23:02:41 -0700

    cleanup of install script

--- resources/public/lib/install.sh
+++ resources/public/lib/install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
 # dotdeploy - Initialization script
-# Registers the machine with dotdeploy, and 
+#
+# Registers the machine with dotdeploy, gathers dependencies, creates a 
+# skeleton directory structure, and installs the cron. 
+#
 DOTDEPLOY_URL='http://localhost:8000/api/'
 MACHINE_UUID=$(uuidgen)
 DOTDEPLOY_DIRECTORY="$HOME/.dotdeploy"
 CLIENT_INSTALL_DIRECTORY="/usr/local/bin"
 CLIENT_NAME="dotdeploy"
-DEPENDENCY_FILENAMES=("logHelper.sh" "cronHelper.sh" "urlHelper.sh" "poller.sh")
+DEPENDENCY_FILENAMES=("logHelper.sh" "cronHelper.sh" "urlHelper.sh" "poller.sh" "pusher.sh")
+DEBUG=true
 
 # Fetches a URL to stdout using the first available of curl, wget.
 #
@@ -15,7 +19,6 @@ DEPENDENCY_FILENAMES=("logHelper.sh" "cronHelper.sh" "urlHelper.sh" "poller.sh")
 #
 # $1: the url to fetch
 function fetchUrl {
-    
     if [ -n $(which curl) ]
     then
         curl -sS $1
@@ -23,11 +26,9 @@ function fetchUrl {
     then
         wget -q -O - $1
     else
-        echo "To install dotdeploy, please ensure your computer has either " \
-             "wget or curl installed."
+        echo "To install dotdeploy, please ensure your computer has either wget or curl installed."
         exit 1 
     fi
-
 }
 
 ## various functions for printing colors
@@ -53,13 +54,12 @@ function logBold {
 
 ## register the machine with dotdeploy
 function registerMachine {
-
     logInfo "Registering this computer on dotdeploy ... "
     FETCH_RESULT=$(fetchUrl "$DOTDEPLOY_URL/machine/index.html")
     
     if [[ $FETCH_RESULT = "true" ]]
     then
-        logSuccess "[success]"
+        logSuccess "[ok]"
     else
         logError "[error - see output below]"
         echo
@@ -73,14 +73,13 @@ function registerMachine {
     logBold $MACHINE_UUID
     echo
     echo
-  
 }
 
 ## create dotdeploy directory structure
 function createDirectoryStructure {
-
-    logInfo "Installing dotdeploy to $DOTDEPLOY_DIRECTORY ... "  
-    if [ -d $DOTDEPLOY_DIRECTORY ]
+    logInfo "Creating directory $DOTDEPLOY_DIRECTORY ... "  
+    
+    if [ -d $DOTDEPLOY_DIRECTORY ] && [[ $DEBUG == false ]]
     then
         logError "[error - directory already exists. exiting.]"
         echo
@@ -88,20 +87,18 @@ function createDirectoryStructure {
         exit 1
     fi
     
-    mkdir $DOTDEPLOY_DIRECTORY
-    mkdir "$DOTDEPLOY_DIRECTORY/lib"
-    mkdir "$DOTDEPLOY_DIRECTORY/originals"
-    mkdir "$DOTDEPLOY_DIRECTORY/tracked"
+    mkdir -p $DOTDEPLOY_DIRECTORY
+    mkdir -p "$DOTDEPLOY_DIRECTORY/lib"
+    mkdir -p "$DOTDEPLOY_DIRECTORY/originals"
+    mkdir -p "$DOTDEPLOY_DIRECTORY/tracked"
     
-    logSuccess "[success]"
+    logSuccess "[ok]"
     echo
     echo
-
 }
 
-##
+## grab shell scripts that the client depends on  
 function fetchDependencies {
-
     logInfo "Fetching dependencies ... "
     echo
     for fileName in "${DEPENDENCY_FILENAMES[@]}"
@@ -112,14 +109,13 @@ function fetchDependencies {
         echo 
     done     
     echo
-    
 }
 
-##
+## fetches the client and attempts to install it into the user's path
 function fetchClient {
-    $tmpFetchLocation = "$DOTDEPLOY_DIRECTORY/$CLIENT_NAME"
-    $targetFilename = "$CLIENT_INSTALL_DIRECTORY/$CLIENT_NAME"
-    
+    tmpFetchLocation="$DOTDEPLOY_DIRECTORY/$CLIENT_NAME"
+    targetFilename="$CLIENT_INSTALL_DIRECTORY/$CLIENT_NAME"
+
     logInfo "Fetching client ... "    
     # fetch the dotdeploy client into the *local* dotdeploy directly
     # then attempt to copy it into /usr/local/bin and 
@@ -127,36 +123,58 @@ function fetchClient {
     chmod +x $tmpFetchLocation
     logSuccess "[ok]"
     echo
-    
-    if [ -x $targetFilename ]    
-    then 
-        return 0
-    fi
 
     # attempt to elevate to sudo for one file copy. if not, provide the user
     # a command they can run on their own    
+    echo
     logInfo "Installing client to $targetFilename. You may be prompted for your password for sudo access."
-    sudo cp -f $targetFilename $tmpFetchLocation
+    echo
+    sudo cp -f $tmpFetchLocation $targetFilename
     
     if [ $? -eq 0 ]
     then
+        echo
         logSuccess "dotdeploy successfully installed to $targetFilename"
         echo
+     
+        rm -f $tmpFetchLocation # cleanup 
     else
-        logInfo "There was an error installing dotdeploy to $targetFilename. " \
-                "To manually install it, run the following command: " 
+        # todo spencer rather than display this message here, return a non-zero
+        # code and then pass that into the finishInstall function with the note
+        # the user needs to run the command before dotdeploy will work. 
         echo
-        logBold "sudo cp -f $tmpFetchLocation $targetLocation"
+        logError "There was an error installing dotdeploy to $targetFilename. To manually install it, run the following command: " 
+        echo
+        logBold "    $ sudo cp -f $tmpFetchLocation $targetFilename"
         echo
     fi
 }
 
+## with dependencies retrieved, setup a cron
+function addCronAndPull {
+    echo
+    logInfo "Installing cron ... " 
+    # todo chris
+    logSuccess "[ok]"
+    echo
+}
+
+## success, present the user with a slightly helpful message
+function finishInstall {
+    echo
+    logSuccess "dotdeploy successfully installed!"
+    echo
+    logBold "To get started, run 'dotdeploy help'"
+    echo
+}
+
 ## with all of the functions defined, run all of the components to initialize
 ## dotdeploy on this machine
-logBold "dotdeploy v0.0 installer"
+logBold "dotdeploy v0.1 installer"
 echo
 createDirectoryStructure
 registerMachine
 fetchDependencies
 fetchClient
+addCronAndPull
 finishInstall
